03182a5a49dd96bebc1cfc3843359b89b8fd3fcc
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-03-26 19:00:44 -0400

    gaming

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,30 @@
 #include <Geode/modify/GameObject.hpp>
 #include <Geode/modify/PlayLayer.hpp>
-#include <list>
-#include <regex>
 
 using namespace geode::prelude;
-using namespace geode::utils::string;
 
-std::list<int> iHateGradients = {503, 504, 505, 1008, 1009, 1010, 1011, 1012, 1013, 1269, 1270, 1271, 1272, 1273, 1274, 1291, 1292, 1293, 1758, 1759, 1760, 1761, 1762, 1763, 1886, 1887, 1888};
-std::list<int> gameplayElements = {10, 11, 12, 13, 35, 36, 45, 46, 47, 67, 84, 99, 101, 111, 140, 141, 200, 201, 202, 203, 286, 287, 660, 745, 746, 747, 748, 1022, 1330, 1331, 1332, 1333, 1334, 1594, 1704, 1751, 1933, 2063, 2064, 2902, 2926, 3004, 3005, 3027 };
+constexpr std::array iHateGradients = {503, 504, 505, 1008, 1009, 1010, 1011, 1012, 1013, 1269, 1270, 1271, 1272, 1273, 1274, 1291, 1292, 1293, 1758, 1759, 1760, 1761, 1762, 1763, 1886, 1887, 1888};
+constexpr std::array gameplayElements = {10, 11, 12, 13, 35, 36, 45, 46, 47, 67, 84, 99, 101, 111, 140, 141, 200, 201, 202, 203, 286, 287, 660, 745, 746, 747, 748, 1022, 1330, 1331, 1332, 1333, 1334, 1594, 1704, 1751, 1933, 2063, 2064, 2902, 2926, 3004, 3005, 3027};
 
-// hide gradients via GameObject (hooking, static_cast, m_fields by dank_meme, matcool, Firee) (string utils suggested by cgytrus)
 class $modify(MyGameObject, GameObject) {
-	struct Fields {
-		bool isGradient = false;
-	};
-	static void onModify(auto & self)
-	{
-		self.setHookPriority("GameObject::createWithFrame", 1000);
-		self.setHookPriority("GameObject::setVisible", 1000);
+	static void onModify(auto & self) {
+		(void) self.setHookPriority("GameObject::setVisible", -3999);
 	}
-	static GameObject* createWithFrame(char const* frameName) {
-		GameObject* gameObject = GameObject::createWithFrame(frameName);
-		if (!PlayLayer::get()) return gameObject;
-		if (typeinfo_cast<PlayerObject*>(gameObject) != nullptr) return gameObject;
-		if (typeinfo_cast<EffectGameObject*>(gameObject) && std::string(frameName).starts_with("secretCoin_")) {
-			return gameObject;
-		}
-		if (!(Mod::get()->getSettingValue<bool>("enabled"))) return gameObject;
-		if (Mod::get()->getSettingValue<int64_t>("hideGlowDecoNew") != 1) return gameObject;
-		if (((strcmp(frameName, "emptyFrame.png") == 0) || (string::contains(frameName, "_gradient_"))))
-			static_cast<MyGameObject*>(gameObject)->m_fields->isGradient = true;
-		return gameObject;
-	}
-	void setVisible(bool p0) {
-		if (typeinfo_cast<PlayerObject*>(this) != nullptr || typeinfo_cast<EffectGameObject*>(this) != nullptr) GameObject::setVisible(p0);
-		else if (m_fields->isGradient) GameObject::setVisible(false);
-		else GameObject::setVisible(p0);
+	void setVisible(bool isVisible) {
+		if (!PlayLayer::get() || !Mod::get()->getSettingValue<bool>("hideGlowDecoFinal")) return GameObject::setVisible(isVisible);
+		if (std::ranges::find(iHateGradients, this->m_objectID) != iHateGradients.end()) return GameObject::setVisible(false);
+		GameObject::setVisible(isVisible);
 	}
 };
 
 // disable glowy objects (idea by TechStudent10, original concept by ItzLever)
 class $modify(MyPlayLayer, PlayLayer) {
-	void addObject(GameObject* p0) {
-		if (Mod::get()->getSettingValue<bool>("enabled")) {
-			bool dontSkip = true;
-			if (Mod::get()->getSettingValue<bool>("disableGradientTriggers") && p0->m_objectID == 2903) dontSkip = false;
-			if (Mod::get()->getSettingValue<int64_t>("hideGlowDecoNew") == 2 && std::find(iHateGradients.begin(), iHateGradients.end(), p0->m_objectID) != iHateGradients.end()) dontSkip = false;
-			if (Mod::get()->getSettingValue<bool>("hideGlowGameplayElements") && std::find(gameplayElements.begin(), gameplayElements.end(), p0->m_objectID) != gameplayElements.end()) p0->m_hasNoGlow = true;
-			if (Mod::get()->getSettingValue<bool>("hideGlowFromBlocks") && !(std::find(gameplayElements.begin(), gameplayElements.end(), p0->m_objectID) != gameplayElements.end())) p0->m_hasNoGlow = true;
-			if (dontSkip) PlayLayer::addObject(p0);
-		}
-		else PlayLayer::addObject(p0);
+	void addObject(GameObject* theObject) {
+		if (!Mod::get()->getSettingValue<bool>("enabled")) return PlayLayer::addObject(theObject);
+		if (Mod::get()->getSettingValue<bool>("disableGradientTriggers") && theObject->m_objectID == 2903) return;
+		const bool inGameplayElements = std::ranges::find(gameplayElements, theObject->m_objectID) != gameplayElements.end();
+		if (Mod::get()->getSettingValue<bool>("hideGlowGameplayElements") && inGameplayElements) theObject->m_hasNoGlow = true;
+		if (Mod::get()->getSettingValue<bool>("hideGlowFromBlocks") && !inGameplayElements) theObject->m_hasNoGlow = true;
+		PlayLayer::addObject(theObject);
 	}
 };
